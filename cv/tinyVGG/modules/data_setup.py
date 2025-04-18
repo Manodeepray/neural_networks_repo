65e1d3c2c0d0b2e9de7795cfb6d1701667998567
Manodeepray <127145195+Manodeepray@users.noreply.github.com>
2025-04-18 17:24:22 +0530

    sum

---
+++ cv/tinyVGG/modules/data_setup.py
@@ -0,0 +1,87 @@
+import os
+import requests
+import zipfile
+from pathlib import Path
+
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+
+
+def get_data():
+    data_path = Path("data/") 
+    image_path = data_path / "pizza_steak_sushi"
+    
+    if image_path.is_dir():
+        print(f"{image_path} already exists.")
+    else:
+        print(f"Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+        
+    with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+        request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+        print("Downloading pizza, steak, sushi data...")
+        f.write(request.content)
+        
+    with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+        print("Unzipping pizza, steak, sushi data...") 
+        zip_ref.extractall(image_path)
+    
+    os.remove(data_path / "pizza_steak_sushi.zip")
+    
+NUM_WORKERS = os.cpu_count()
+
+def create_dataloaders(
+    train_dir: str, 
+    test_dir: str, 
+    transform: transforms.Compose, 
+    batch_size: int, 
+    num_workers: int=NUM_WORKERS
+):
+  """Creates training and testing DataLoaders.
+
+  Takes in a training directory and testing directory path and turns
+  them into PyTorch Datasets and then into PyTorch DataLoaders.
+
+  Args:
+    train_dir: Path to training directory.
+    test_dir: Path to testing directory.
+    transform: torchvision transforms to perform on training and testing data.
+    batch_size: Number of samples per batch in each of the DataLoaders.
+    num_workers: An integer for number of workers per DataLoader.
+
+  Returns:
+    A tuple of (train_dataloader, test_dataloader, class_names).
+    Where class_names is a list of the target classes.
+    Example usage:
+      train_dataloader, test_dataloader, class_names = \
+        = create_dataloaders(train_dir=path/to/train_dir,
+                             test_dir=path/to/test_dir,
+                             transform=some_transform,
+                             batch_size=32,
+                             num_workers=4)
+  """
+  # Use ImageFolder to create dataset(s)
+  train_data = datasets.ImageFolder(train_dir, transform=transform)
+  test_data = datasets.ImageFolder(test_dir, transform=transform)
+
+  # Get class names
+  class_names = train_data.classes
+
+  # Turn images into data loaders
+  train_dataloader = DataLoader(
+      train_data,
+      batch_size=batch_size,
+      shuffle=True,
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+  test_dataloader = DataLoader(
+      test_data,
+      batch_size=batch_size,
+      shuffle=False, # don't need to shuffle test data
+      num_workers=num_workers,
+      pin_memory=True,
+  )
+
+  return train_dataloader, test_dataloader, class_names
